0a0a218450851248240515e678ccbc75be678e0a
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-01-09 14:11:41 +0100

    Refactoring session state machine

--- src/nonblock.rs
+++ src/nonblock.rs
@@ -14,9 +14,8 @@ pub trait ReadNonblocking: Read {
 
     fn read_nonblocking(&mut self, buf: &mut [u8]) -> IoStatus {
         match self.read(buf) {
-            // If we get zero bytes read as a return value, it means the peer has
-            // performed an orderly shutdown.
-            Ok(0) => IoStatus::Shutdown,
+            // If we get zero bytes read as a return value, it means we are still
+            // in the process of handshake.
             Ok(len) => IoStatus::Success(len),
             Err(err) if err.kind() == ErrorKind::WouldBlock => IoStatus::WouldBlock,
             Err(err) => IoStatus::Err(err),
@@ -42,7 +41,12 @@ impl ReadNonblocking for socket2::Socket {
 pub trait WriteNonblocking: Write {
     fn set_write_nonblocking(&mut self, timeout: Option<Duration>) -> Result<()>;
 
+    fn can_write(&self) -> bool;
+
     fn write_nonblocking(&mut self, buf: &[u8]) -> IoStatus {
+        if !self.can_write() {
+            return IoStatus::Err(ErrorKind::NotConnected.into());
+        }
         if buf.is_empty() {
             return IoStatus::Success(0);
         }
@@ -64,10 +68,16 @@ pub trait WriteNonblocking: Write {
     }
 }
 
+// TODO: Do a dedicated non-blocking TcpStream type
 impl WriteNonblocking for TcpStream {
     fn set_write_nonblocking(&mut self, timeout: Option<Duration>) -> Result<()> {
         self.set_nonblocking(true)?;
         self.set_write_timeout(timeout)
+    }
+
+    fn can_write(&self) -> bool {
+        // In the reality we might not, but this would be decided on the levels above
+        true
     }
 }
 
@@ -76,5 +86,10 @@ impl WriteNonblocking for socket2::Socket {
     fn set_write_nonblocking(&mut self, timeout: Option<Duration>) -> Result<()> {
         self.set_nonblocking(true)?;
         self.set_write_nonblocking(timeout)
+    }
+
+    fn can_write(&self) -> bool {
+        // In the reality we might not, but this would be decided on the levels above
+        true
     }
 }
