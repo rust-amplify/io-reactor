eecbde379c3730c87af831e76a7b5154bbd598a4
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2023-01-10 14:55:13 +0100

    Fix timeouts once more

--- src/timeouts.rs
+++ src/timeouts.rs
@@ -3,7 +3,10 @@ use std::time::{Duration, Instant};
 /// Manages timers and triggers timeouts.
 #[derive(Debug)]
 pub struct TimeoutManager<K = ()> {
-    timeouts: Vec<(K, Instant)>,
+    /// Timeouts are durations since the UNIX epoch.
+    timeouts: Vec<(K, Duration)>,
+    /// Threshold below which a timeout can't be added if another timeout is set
+    /// within the range of the threshold.
     threshold: Duration,
 }
 
@@ -28,37 +31,38 @@ impl<K> TimeoutManager<K> {
         self.timeouts.is_empty()
     }
 
-    /// Register a new timeout with an associated key and wake-up time.
+    /// Register a new timeout with an associated key and wake-up time from a
+    /// UNIX time epoch.
     ///
     /// ```
-    /// use std::time::{Duration, Instant};
+    /// use std::time::{Duration};
     /// use reactor::TimeoutManager;
     ///
     /// let mut tm = TimeoutManager::new(Duration::from_secs(1));
-    /// let now = Instant::now();
     ///
-    /// let registered = tm.register(0xA, now + Duration::from_secs(8));
+    /// // Sets timeout from Instant::now();
+    /// let registered = tm.register(0xA, Duration::from_secs(8));
     /// assert!(registered);
     ///
-    /// let registered = tm.register(0xB, now + Duration::from_secs(9));
+    /// let registered = tm.register(0xB, Duration::from_secs(9));
     /// assert!(registered);
     /// assert_eq!(tm.len(), 2);
     ///
-    /// let registered = tm.register(0xC, now + Duration::from_millis(9541));
+    /// let registered = tm.register(0xC, Duration::from_millis(9541));
     /// assert!(!registered);
     ///
-    /// let registered = tm.register(0xC, now + Duration::from_millis(9999));
+    /// let registered = tm.register(0xC, Duration::from_millis(9999));
     /// assert!(!registered);
     /// assert_eq!(tm.len(), 2);
     /// ```
-    pub fn register(&mut self, key: K, time: Instant) -> bool {
+    pub fn register(&mut self, key: K, time: Duration) -> bool {
         // If this timeout is too close to a pre-existing timeout,
         // don't register it.
         if self.timeouts.iter().any(|(_, t)| {
             if *t < time {
-                time.duration_since(*t) < self.threshold
+                time - *t < self.threshold
             } else {
-                t.duration_since(time) < self.threshold
+                *t - time < self.threshold
             }
         }) {
             return false;
@@ -74,16 +78,16 @@ impl<K> TimeoutManager<K> {
     /// to be reached.  Returns `None` if there are no timeouts.
     ///
     /// ```
-    /// # use std::time::{Duration, Instant};
+    /// # use std::time::{Duration};
     /// use reactor::TimeoutManager;
     ///
     /// let mut tm = TimeoutManager::new(Duration::from_secs(0));
-    /// let mut now = Instant::now();
     ///
-    /// tm.register(0xA, now + Duration::from_millis(16));
-    /// tm.register(0xB, now + Duration::from_millis(8));
-    /// tm.register(0xC, now + Duration::from_millis(64));
+    /// tm.register(0xA, Duration::from_millis(16));
+    /// tm.register(0xB, Duration::from_millis(8));
+    /// tm.register(0xC, Duration::from_millis(64));
     ///
+    /// let mut now = Duration::from_millis(0);
     /// // We need to wait 8 millis to trigger the next timeout (1).
     /// assert!(tm.next(now) <= Some(Duration::from_millis(8)));
     ///
@@ -93,9 +97,8 @@ impl<K> TimeoutManager<K> {
     /// // Now we don't need to wait as long!
     /// assert!(tm.next(now).unwrap() <= Duration::from_millis(7));
     /// ```
-    pub fn next(&self, now: impl Into<Instant>) -> Option<Duration> {
+    pub fn next(&self, now: impl Into<Duration>) -> Option<Duration> {
         let now = now.into();
-
         self.timeouts.last().map(|(_, t)| {
             if *t >= now {
                 *t - now
@@ -107,7 +110,7 @@ impl<K> TimeoutManager<K> {
 
     /// Given a specific time, add to the input vector keys that
     /// have timed out by that time. Returns the number of keys that timed out.
-    pub fn check(&mut self, time: Instant, fired: &mut Vec<K>) -> usize {
+    pub fn check(&mut self, time: Duration, fired: &mut Vec<K>) -> usize {
         let before = fired.len();
 
         while let Some((k, t)) = self.timeouts.pop() {
@@ -125,25 +128,24 @@ impl<K> TimeoutManager<K> {
     /// have timed out. Returns the number of keys that timed out.
     ///
     /// ```
-    /// use std::time::{Duration, Instant};
+    /// use std::time::{Duration};
     /// use reactor::TimeoutManager;
     ///
     /// let mut tm = TimeoutManager::new(Duration::from_secs(0));
-    /// let now = Instant::now();
     ///
-    /// tm.register(0xA, now + Duration::from_millis(8));
-    /// tm.register(0xB, now + Duration::from_millis(16));
-    /// tm.register(0xC, now + Duration::from_millis(64));
-    /// tm.register(0xD, now + Duration::from_millis(72));
+    /// tm.register(0xA, Duration::from_millis(8));
+    /// tm.register(0xB, Duration::from_millis(16));
+    /// tm.register(0xC, Duration::from_millis(64));
+    /// tm.register(0xD, Duration::from_millis(72));
     ///
     /// let mut timeouts = Vec::new();
     ///
-    /// assert_eq!(tm.check(now + Duration::from_millis(21), &mut timeouts), 2);
+    /// assert_eq!(tm.check(Duration::from_millis(21), &mut timeouts), 2);
     /// assert_eq!(timeouts, vec![0xA, 0xB]);
     /// assert_eq!(tm.len(), 2);
     /// ```
     pub fn check_now(&mut self, fired: &mut Vec<K>) -> usize {
-        self.check(Instant::now(), fired)
+        self.check(Duration::from_millis(0), fired)
     }
 }
 
@@ -154,15 +156,15 @@ mod tests {
     #[test]
     fn test_wake() {
         let mut tm = TimeoutManager::new(Duration::from_secs(0));
-        let now = Instant::now();
 
-        tm.register(0xA, now + Duration::from_millis(8));
-        tm.register(0xB, now + Duration::from_millis(16));
-        tm.register(0xC, now + Duration::from_millis(64));
-        tm.register(0xD, now + Duration::from_millis(72));
+        tm.register(0xA, Duration::from_millis(8));
+        tm.register(0xB, Duration::from_millis(16));
+        tm.register(0xC, Duration::from_millis(64));
+        tm.register(0xD, Duration::from_millis(72));
 
         let mut timeouts = Vec::new();
 
+        let now = Duration::from_millis(0);
         assert_eq!(tm.check(now, &mut timeouts), 0);
         assert_eq!(timeouts, vec![]);
         assert_eq!(tm.len(), 4);
