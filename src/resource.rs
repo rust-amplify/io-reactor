7601ca0ffb71ef75156abac01537ac49a67e1690
Dr Maxim Orlovsky <orlovsky@cyphernet.io>
2025-04-17 11:21:46 +0200

    chore: update licensing information

--- src/resource.rs
+++ src/resource.rs
@@ -2,24 +2,22 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-// Written in 2021-2023 by
+// Written in 2021-2025 by
 //     Dr. Maxim Orlovsky <orlovsky@ubideco.org>
 //     Alexis Sellier <alexis@cloudhead.io>
 //
-// Copyright 2022-2023 UBIDECO Institute, Switzerland
-// Copyright 2021 Alexis Sellier <alexis@cloudhead.io>
+// Copyright 2022-2025 UBIDECO Labs, InDCS, Lugano, Switzerland. All Rights reserved.
+// Copyright 2021-2023 Alexis Sellier <alexis@cloudhead.io>. All Rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
 
 use std::convert::Infallible;
 use std::fmt::Debug;
